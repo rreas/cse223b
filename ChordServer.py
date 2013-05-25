dfe70dbc12221b552403a7025c634e86459a96df
conniec <spiritgreen@gmail.com>
2013-05-25 16:46:33 -0700

    remove cruft :)

--- ChordServer.py
+++ ChordServer.py
@@ -1,12 +1,9 @@
 import sys
 sys.path.append('./gen-py')
-from hashlib import md5
-from bisect import bisect
 from time import sleep
 from math import pow
 
 # Thrift imports
-from thrift import Thrift
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
@@ -23,8 +20,9 @@ from helpers import *
 FINGER_TABLE_LENGTH = 128
 MAX = pow(2, FINGER_TABLE_LENGTH)
 
-class ChordServer(KeyValueStore.Iface): # probably need the thrift interface inside the parantheses here.
-    
+
+class ChordServer(KeyValueStore.Iface):  # probably need the thrift interface inside the parantheses here.
+
     # If chord_name and chord_port are not given, this is the first node in Chord ring.
     # Otherwise, connect to the chord server to find the position.
     def __init__(self, hostname, port, chord_name=None, chord_port=None):
@@ -53,7 +51,6 @@ class ChordServer(KeyValueStore.Iface): # probably need the thrift interface ins
             self.initialize_finger_tables()
 
         self.initialize_threads()
-        
 
     def initialize(self):
         with remote(self.successor) as client:
@@ -82,7 +79,7 @@ class ChordServer(KeyValueStore.Iface): # probably need the thrift interface ins
             self.finger_hash_table.append(self.hashcode)
 
     def initialize_threads(self):
-        stabilizer = threading2.Thread(target = self.stabilize)
+        stabilizer = threading2.Thread(target=self.stabilize)
         stabilizer.daemon = True
         stabilizer.start()
 
@@ -91,7 +88,6 @@ class ChordServer(KeyValueStore.Iface): # probably need the thrift interface ins
         fixer.start()'''
 
     def get_successor_for_key(self, hashcode):
-        
         #print "get_successor_for_key ", hashcode
         '''if type(hashcode) == str:
             hashcode_int = int((hashcode))
@@ -134,7 +130,6 @@ class ChordServer(KeyValueStore.Iface): # probably need the thrift interface ins
             hashcode_int = int((hashcode))
         else:
             hashcode_int = hashcode'''
-        hashcode_int = int(hashcode)
         # dummy for now.
         data_response = DataResponse()
         data_response.kvstore = {}
@@ -146,10 +141,9 @@ class ChordServer(KeyValueStore.Iface): # probably need the thrift interface ins
         data_response.status = ChordStatus.OK
         return data_response
 
-
     def get_predecessor(self):
         return self.predecessor
-        
+
     def get_successor(self):
         return self.successor
 
@@ -163,7 +157,6 @@ class ChordServer(KeyValueStore.Iface): # probably need the thrift interface ins
             response.status = ChordStatus.OK
             response.value = self.kvstore[key]
             return response
-
 
         master_node = self.get_successor_for_key(str(hashedKey))
         with remote(master_node) as client:
@@ -192,7 +185,7 @@ class ChordServer(KeyValueStore.Iface): # probably need the thrift interface ins
 
     def notify(self, node):
         # TODO: Probably need a check to see if it is truly the predecessor (see Chord)
-        print "%s thinks it is our predecessor" %(node)
+        print "%s thinks it is our predecessor" % (node)
         self.predecessor = node
         return ChordStatus.OK
 
@@ -207,10 +200,10 @@ class ChordServer(KeyValueStore.Iface): # probably need the thrift interface ins
                 x = self.predecessor
 
             if x is not None and x != self.node_key:
-                # TODO: Do we need better checks or does it get stabilized eventually to the right node.  
+                # TODO: Do we need better checks or does it get stabilized eventually to the right node.
                 # print "New successor ", x
                 self.successor = x
-                print "notifying %s that it is our successor" %(x)
+                print "notifying %s that it is our successor" % (x)
                 with remote(x) as client:
                     status = client.notify(self.node_key)
                 # TODO check status and take action.
