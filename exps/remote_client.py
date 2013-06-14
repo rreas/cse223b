3c8d40ce3f892b170317bae905c4d5feda896eb6
Russell Reas <russell.reas@gmail.com>
2013-06-14 13:53:53 -0700

    experiments on finger tables.

---
+++ exps/remote_client.py
@@ -0,0 +1,63 @@
+import sys
+sys.path.append('.')
+sys.path.append('tests/')
+import string
+import random
+import threading
+import multiprocessing
+
+from nose.tools import *
+from time import sleep, time
+import numpy as np
+from ChordServer import *
+from KeyValue import KeyValueStore
+from KeyValue.ttypes import KeyValueStatus, ChordStatus
+
+# Thrift.
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+
+from contextlib import contextmanager
+from helpers import encode_node
+from TestHelpers import *
+
+# Characters for data.
+data_source = string.ascii_lowercase + string.digits
+
+def fake_data(length):
+    data = [random.choice(data_source) for x in range(length)]
+    return ''.join(data)
+
+num_data_elements = 1000
+
+# Create a bunch of fake data.
+all_data = set()
+while(len(all_data) < num_data_elements):
+    all_data.add(fake_data(10))
+
+serverip = sys.argv[1]
+serverport = int(sys.argv[2])
+
+# Store my data on server.
+with connect(serverport, serverip) as client:
+    for s in all_data:
+        client.put(s, s)
+        sys.stdout.write('.')
+        sys.stdout.flush()
+
+# Read 1k keys.
+while True:
+    reads = np.zeros((len(all_data),))
+    with connect(serverport, serverip) as client:
+        for ix, s in enumerate(all_data):
+            st = time()
+            resp = client.get(s)
+            fn = time()
+            reads[ix] = fn-st
+        print "done."
+    reads *= 1000
+    print "Read 1k keys (mean, max):", np.mean(reads), np.max(reads)
+
---
+++ exps/remote_server.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append('.')
+sys.path.append('tests/')
+import string
+import random
+import threading
+import multiprocessing
+
+from nose.tools import *
+from time import sleep, time
+import numpy as np
+from ChordServer import *
+from KeyValue import KeyValueStore
+from KeyValue.ttypes import KeyValueStatus, ChordStatus
+
+# Thrift.
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+
+from contextlib import contextmanager
+from helpers import encode_node
+from TestHelpers import *
+
+num_data_elements = 1000
+myport = random.randint(20000, 40000)
+
+otherip = None
+otherport = None
+if len(sys.argv) > 1:
+    otherip = sys.argv[1]
+    otherport = int(sys.argv[2])
+
+# Start server and add to ring if needed.
+handler = ChordServer('localhost', myport, otherip, otherport)
+processor = KeyValueStore.Processor(handler)
+transport = TSocket.TServerSocket('localhost', myport)
+tfactory = TTransport.TBufferedTransportFactory()
+pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+server = TServer.TThreadedServer(processor, transport, tfactory, pfactory)
+print "starting server on port", myport, "..."
+server.serve()
+
